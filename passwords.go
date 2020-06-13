3e9174a10716b1553d96f8f0b219cbb37f3b862c
salvorapisarda <salvo.rapisarda@sainsbury.co.uk>
2020-06-13 15:52:33 +0100

    add 100 iteration

--- passwords.go
+++ passwords.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 func main() {
-	password := "secret"
-	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+	start := time.Now()
+	fmt.Println(start)
+	for i := 0; i < 100; i++ {
+		password := "secret"
+		hash, _ := HashPassword(password) // ignore error for the sake of simplicity
 
-	fmt.Println("Password:", password)
-	fmt.Println("Hash:    ", hash)
+		fmt.Println("Password:", password)
+		fmt.Println("Hash:    ", hash)
 
-	match := CheckPasswordHash(password, hash)
-	fmt.Println("Match:   ", match)
+		match := CheckPasswordHash(password, hash)
+		fmt.Println("Match:   ", match)
+
+	}
+	end := time.Now()
+	elapsed := end.Sub(start).Seconds()
+	fmt.Printf("time %s elapsed %d \n", time.Now(), elapsed)
 }
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 2)
 	return string(bytes), err
 }
 
@@ -25,5 +34,3 @@ func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
-
-
